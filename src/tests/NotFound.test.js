0dc59117442ff2cfa38180656474f650edfa96a3
vtrtga <vitor.ypvalim@gmail.com>
2022-06-20 10:03:47 -0300

    new commit

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,26 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import { NotFound } from '../pages';
+import renderWithRouter from '../RenderWithRouter';
+
+describe('Testando componente NotFound.js', () => {
+  it('Teste se a página contém um h2 com o texto Page not found', () => {
+    renderWithRouter(<NotFound />);
+    const h2 = screen.getByRole('heading', {
+      name: /page requested not found/i,
+      level: 2,
+    });
+
+    expect(h2).toBeInTheDocument();
+  });
+
+  it('Teste se a página mostra a imagem com a url', () => {
+    renderWithRouter(<NotFound />);
+    const img = screen.getByRole('img', {
+      name: /pikachu crying because the page requested was not found/i,
+    });
+    const url = 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif';
+
+    expect(img).toHaveProperty('src', url);
+  });
+});
--- src/tests/Pokedex.test.js
+++ src/tests/Pokedex.test.js
@@ -1,1 +1,69 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../RenderWithRouter';
+import App from '../App';
+import pokemons from '../data';
+
+describe('Teste o componente <Pokedex.js />', () => {
+  it('Testa se texto "Encountered pokémons" é mostrado',
+    () => {
+      renderWithRouter(<App />);
+      const head = screen.getByRole('heading', {
+        level: 2,
+        name: /Encountered pokémons/i,
+      });
+      expect(head).toBeInTheDocument();
+    });
+
+  it('Teste se é exibido o próximo pokémon da lista quando o botão é clicado',
+    () => {
+      renderWithRouter(<App />);
+      const button = screen.getByRole('button', {
+        name: 'Próximo pokémon',
+      });
+      pokemons.forEach(({ name }) => {
+        expect(screen.getByText(name)).toBeInTheDocument();
+        userEvent.click(button);
+      });
+    });
+
+  it('Teste se é mostrado apenas um pokémon por vez', () => {
+    renderWithRouter(<App />);
+    const item = screen.getByText(pokemons[0].name);
+    expect(item).toBeInTheDocument();
+  });
+
+  it('Teste se a Pokédex tem os botões de filtro', () => {
+    renderWithRouter(<App />);
+    const filterButtons = screen.getAllByTestId('pokemon-type-button');
+
+    filterButtons.forEach((button) => {
+      expect(button).toBeInTheDocument();
+      const btn = screen.getByRole('button', { name: button.textContent });
+      userEvent.click(btn);
+
+      const name = screen.getByTestId('pokemon-name');
+      const type = screen.getByTestId('pokemon-type');
+      const next = screen.getByTestId('next-pokemon');
+
+      const getPokemonsByType = pokemons.filter((pokemon) => (
+        pokemon.type === button.textContent
+      ));
+
+      getPokemonsByType.forEach((pokemon) => {
+        expect(pokemon.type).toEqual(button.textContent);
+        expect(type.textContent).toEqual(pokemon.type);
+        expect(name.textContent).toEqual(pokemon.name);
+        userEvent.click(next);
+      });
+    });
+  });
+
+  it('Teste se a Pokédex contém um botão para resetar o filtro', () => {
+    renderWithRouter(<App />);
+    const buttonAll = screen.getByRole('button', { name: /all/i });
+    expect(buttonAll).toBeInTheDocument();
+    userEvent.click(buttonAll);
+  });
+});
