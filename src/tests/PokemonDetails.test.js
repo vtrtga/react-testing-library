b32e3ef207e22292996148b8c1976cf8eaa2ed66
vtrtga <vitor.ypvalim@gmail.com>
2022-06-21 11:24:15 -0300

    req 7

--- src/tests/PokemonDetails.test.js
+++ src/tests/PokemonDetails.test.js
@@ -1,1 +1,65 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import RenderWithRouter from '../RenderWithRouter';
+import App from '../App';
+
+const pikachuInfo = '/pokemons/25';
+
+describe('Teste o componente <PokemonDetails.js />', () => {
+  beforeEach(() => {
+    RenderWithRouter(<App />);
+  });
+
+  it('Teste se as informações detalhadas do pokémon selecionado são mostradas',
+    () => {
+      userEvent.click(screen.getByRole('button', { name: /electric/i }));
+      userEvent.click(screen.getByRole('link', { name: /more details/i }));
+
+      const pikachuDetails = screen.getByText(/pikachu details/i);
+      expect(pikachuDetails).toBeInTheDocument();
+
+      const summary = screen.getByText(/summary/i);
+      expect(summary).toBeInTheDocument();
+
+      const pikachuText = screen.getByText(/This intelligent Pokémon roasts hard/);
+      expect(pikachuText).toBeInTheDocument();
+    });
+
+  it('Teste se existe uma seção com os mapas contendo as localizações do pokémon:',
+    () => {
+      const { history } = RenderWithRouter(<App />);
+      history.push(pikachuInfo);
+
+      const locations = screen.getByRole('heading',
+        { name: /Game Locations of Pikachu/ });
+      expect(locations).toBeInTheDocument();
+
+      const divLocationsImage = document.querySelectorAll('.pokemon-habitat');
+      expect(() => (divLocationsImage.length > 0)).toBeTruthy();
+
+      const locationImage = screen.getAllByRole('img', { name: /pikachu location/i });
+      locationImage.forEach((element) => {
+        expect(element).toHaveAttribute('src', `${element.src}`);
+        expect(element).toHaveProperty('alt', `${element.alt}`);
+      });
+    });
+
+  it('Teste se o usuário pode favoritar um pokémon através da página de detalhes', () => {
+    const { history } = RenderWithRouter(<App />);
+    history.push(pikachuInfo);
+
+    const checkBox = screen.getByRole('checkbox', { name: /pokémon favoritado\?/i });
+    expect(checkBox).toBeInTheDocument();
+
+    userEvent.click(checkBox);
+    const startIcon = screen.getByRole('img', { name: /pikachu is marked as favorite/i });
+    expect(startIcon).toBeInTheDocument();
+
+    userEvent.click(checkBox);
+    expect(startIcon).not.toBeInTheDocument();
+
+    const labelText = screen.getByText(/pokémon favoritado\?/i);
+    expect(labelText).toBeInTheDocument();
+  });
+});
